1d0bd8e27e2880ec07f002b57818e5bfdeb897f5
muhammadimran021 <muhammad_imran01@hotmail.com>
2017-01-12 00:25:01 +0500

    commit blogfile

---
+++ ChatApplication/app/src/main/java/com/example/babarmustafa/chatapplication/User_Profile/postView/BlogViewHolder.java
@@ -0,0 +1,34 @@
+package com.example.babarmustafa.chatapplication.User_Profile.postView;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.babarmustafa.chatapplication.R;
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by muhammad imran on 11-Jan-17.
+ */
+
+public class BlogViewHolder extends RecyclerView.ViewHolder {
+    View view;
+    public BlogViewHolder(View itemView) {
+        super(itemView);
+        view = itemView;
+    }
+    public void setTitle(String title){
+
+        TextView post_title = (TextView) view.findViewById(R.id.User_status);
+        post_title.setText(title);
+    }
+
+    public void setImage(Context context, String image){
+
+        ImageView post_image = (ImageView) view.findViewById(R.id.User_post_image);
+        Picasso.with(context).load(image).into(post_image);
+
+    }
+}
